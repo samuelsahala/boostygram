9b3419ee620293907618853181cc6202f81c584e
Shmuel <samuel.sahala@ricult.com>
2020-10-17 14:57:09 +0300

    style component init add global theme

---
+++ src/utils/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors:{
+    main:'#FCFCFC',
+    mainDark:'#2D323B',
+    mainLight:'#272F3F',
+    mainLighter:'#364259',
+    textColor:'#333',
+    whiteColor:'#fff',
+    shadow:'rgba(0,0,0,.2)',
+  },
+  mediaQueries:{
+    smallest:`(max-width:25em)`,
+    small:`(max-width:37.5em)`,
+    medium:`(max-width:56em)`,
+    large:`(max-width:80em)`,
+    largest:`(max-width:90em)`,
+  }
+};
+
+export default theme;
